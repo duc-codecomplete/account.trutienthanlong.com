0336622bd3891a3cb9716f82186582136a76a818
Duc Nguyen Do Viet <duc.nguyen@codecomplete.jp>
2024-06-25 17:07:20 +0700

    login

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,45 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta20
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
 <html lang="en">
 
 <head>
-    <title>Tru Tiên Việt Nam - Hoạ Ảnh Giáng Lâm</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Tru Tiên Thần Long - Phiên bản 18 class</title>
+    <!-- CSS files -->
+    <link href="/ui/dist/css/tabler.min.css?1692870487" rel="stylesheet" />
+    <link href="/ui/dist/css/tabler-flags.min.css?1692870487" rel="stylesheet" />
+    <link href="/ui/dist/css/tabler-payments.min.css?1692870487" rel="stylesheet" />
+    <link href="/ui/dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet" />
+    <link href="/ui/dist/css/demo.min.css?1692870487" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
 
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
 
-    <meta name="description" content="Tru Tiên Việt Nam - Hoạ Ảnh Giáng Lâm">
-    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="icon" type="image/x-icon" href="/assets/logo2.png">
-
-    <!-- FontAwesome JS-->
-    <script defer src="/assets/new/assets/plugins/fontawesome/js/all.min.js"></script>
-
-    <!-- App CSS -->
-    <link id="theme-style" rel="stylesheet" href="/assets/new/assets/css/portal.css">
-
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
 </head>
 
-<body class="app app-login p-0">
-    <div class="row g-0 app-auth-wrapper">
-        <div class="col-12 col-md-7 col-lg-6 auth-main-col text-center p-5">
-            @yield('content')
-        </div>
-        <!--//auth-main-col-->
-        <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
-            <div class="auth-background-holder">
+<body class=" d-flex flex-column">
+    <script src="/ui/dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page page-center">
+        <div class="container container-tight py-4">
+            <div class="text-center mb-4">
+                <a href="/" class="navbar-brand navbar-brand-autodark">
+                    <img src="/ui/logo2.png" width="174" height="200" alt="Tabler" class="" style="height:auto">
+                </a>
             </div>
-            <div class="auth-background-mask"></div>
+            <div class="content">
+                @yield('content')
+            </div>
         </div>
-        <!--//auth-background-col-->
-
     </div>
-    <!--//row-->
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="/ui/dist/js/tabler.min.js?1692870487" defer></script>
+    <script src="/ui/dist/js/demo.min.js?1692870487" defer></script>
 </body>
 
 </html>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,78 +1,62 @@
 @extends('layouts.auth')
 @section('content')
-<div class="d-flex flex-column align-content-end">
-    <div class="app-auth-body mx-auto">
-        <div class="app-auth-branding mb-4"><a class="app-logo" href="index.html"><img class="me-2"
-                    src="/assets/logo2.png" alt="logo"></a></div>
-        <h2 class="auth-heading text-center mb-4">Đăng Ký Tài Khoản</h2>
+<div class="card card-md">
+    <div class="card-body">
+        <h2 class="h2 text-center mb-4">Đăng Ký</h2>
 
-        @if ($errors->any())
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <small>{{ $errors->first() }}</small>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-        @if(Session::has('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <small>{{ Session::get('error') }}</small>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-        @if(Session::has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <small>{{ Session::get('success') }}</small>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-        <div class="auth-form-container text-start mx-auto">
-            <form class="auth-form auth-signup-form" action="" method="POST">
-                @csrf
-                <div class="email mb-3">
-                    <label class="sr-only" for="signup-email">Tên đăng nhập</label>
-                    <input value="{{ old('login') }}" id="signup-name" name="login" type="text"
-                        class="form-control signup-name" placeholder="Tên đăng nhập" requireds="requireds">
-                </div>
-                <div class="email mb-3">
-                    <label class="sr-only" for="signup-email">Mật khẩu</label>
-                    <input id="signup-email" name="passwd" type="password" class="form-control signup-email"
-                        placeholder="Mật khẩu" requireds="requireds">
-                </div>
-                <div class="password mb-3">
-                    <label class="sr-only" for="signup-password">Nhập lại mật khẩu</label>
-                    <input id="signup-password" name="passwdConfirm" type="password"
-                        class="form-control signup-password" placeholder="Nhập lại mật khẩu" requireds="requireds">
-                </div>
-                <div class="email mb-3">
-                    <label class="sr-only" for="signup-email">Email</label>
-                    <input value="{{ old('email') }}" id="signup-name" name="email" type="email"
-                        class="form-control signup-name" placeholder="Địa chỉ email" requireds="requireds">
-                </div>
-                <!--//extra-->
-
-                <div class="text-center">
-                    <button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">Đăng ký</button>
-                </div>
-            </form>
-            <!--//auth-form-->
-
-            <div class="auth-option text-center pt-5">Đã có tài khoản? <a class="text-link" href="/dang-nhap">Đăng
-                    nhập</a></div>
-        </div>
-        <!--//auth-form-container-->
-
-
-
+        <form action="" method="POST" autocomplete="off" novalidate>
+            @csrf
+            @if ($errors->any())
+            <div class="alert alert-important alert-danger alert-dismissible" role="alert">
+                <small>{{ $errors->first() }}</small>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+            @if(Session::has('error'))
+            <div class="alert alert-important alert-danger alert-dismissible" role="alert">
+                <small>{{ Session::get('error') }}</small>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+            @if(Session::has('success'))
+            <div class="alert alert-important alert-success alert-dismissible" role="alert">
+                <small>{{ Session::get('success') }}</small>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+            <div class="mb-3">
+                <label class="form-label">Tên đăng nhập</label>
+                <input type="" class="form-control" placeholder="Tên đăng nhập" autocomplete="off" name="login"
+                    required>
+            </div>
+            <div class="mb-2">
+                <label class="form-label">
+                    Mật khẩu
+                </label>
+                <input type="password" class="form-control" autocomplete="off"
+                    placeholder="Mật khẩu" required="required" name="passwd">
+            </div>
+            <div class="mb-2">
+                <label class="form-label">
+                    Nhập lại mật khẩu
+                </label>
+                <input type="password" class="form-control" autocomplete="off"
+                    placeholder="Nhập lại mật khẩu" required="required" name="passwdConfirm">
+            </div>
+            <div class="mb-2">
+                <label class="form-label">
+                    Email
+                </label>
+                <input type="email" class="form-control" autocomplete="off"
+                    placeholder="Email" required="required" name="email">
+            </div>
+            <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">Đăng Ký</button>
+            </div>
+        </form>
     </div>
-    <!--//auth-body-->
-
-    <footer class="app-auth-footer">
-        <div class="container text-center py-3">
-            <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-            <small class="copyright">Copyright 2024 © <span class="sr-only">love</span><i class="fas fa-heart"
-                    style="color: #fb866a;"></i> by TruTien.Vn</small>
-
-        </div>
-    </footer>
-    <!--//app-auth-footer-->
+</div>
+<div class="text-center text-secondary mt-3">
+    Đã có tài khoản? <a href="/dang-nhap" tabindex="-1">Đăng Nhập</a>
 </div>
 @endsection
